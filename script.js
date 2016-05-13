3cca7af9d25102b9a7ae009aa0fbcee026851fbe
Leah <leahpwatt@gmail.com>
2016-05-13 15:49:10 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+var app = angular.module("ourApp", ["ngRoute"]);
+	app.config(function($routeProvider){
+		$routeProvider
+		.when("/",
+		{
+			controller: "homeController",
+			templateUrl: "partials/landing.html"
+		})
+		.when("/input"
+		{
+			controller: "inputController",
+			templateUrl: "partials/input.html"
+		})
+		.when("/output"
+		{
+			controller: "outputController",
+			templateUrl: "partials/output.html"
+		})
+		.when("/api"
+		{
+			controller: "apiController",
+			templateUrl: "partials/api.html"
+		})
+		.otherwise({redirectTo: "/"});
+	});
