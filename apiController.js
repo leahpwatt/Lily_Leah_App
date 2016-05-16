4630286c6c7f28531c81389d8c44f5a459bc5beb
Leah <leahpwatt@gmail.com>
2016-05-16 17:25:56 -0400

    basic styles

--- apiController.js
+++ apiController.js
@@ -2,12 +2,7 @@ var app=angular.module("routerApp");
 
 app.controller("apiController", function($scope, apiService, $location){
 	$scope.getRandomQuotes = function(){
-		apiService.chooseRandomQuote($scope.wiseQuotes);
+		apiService.chooseRandomQuote;
 		$location.path("home/apiOutput")
 	}
-
-	// apiService.getRandomQuotes().then(function(data){
-	// 	console.log(data.data.quote);
-	// 	console.log(data.data.author);
-	// });
 })
--- apiOutputController.js
+++ apiOutputController.js
@@ -4,8 +4,6 @@ app.controller("apiOutputController", function($scope, apiService){
 	apiService.getRandomQuotes().then(function(data){
 		$scope.quotes = data.data.quote;		
 		$scope.author = data.data.author;		
-	console.log(data.data.quote);
-		console.log(data.data.author);
 	});
 
 
